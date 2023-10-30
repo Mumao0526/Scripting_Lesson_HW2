0db119e2898c993e22697df80d378b1dc733fd4b
Mumao0526 <75149750+Mumao0526@users.noreply.github.com>
2023-10-30 20:36:39 +0800

    Update and rename star.py to 9a917056_01.py

--- 9a917056_01.py
+++ 9a917056_01.py
@@ -1,8 +1,10 @@
 num = int(input('請輸入數量:'))
+
 for i in range(1,num,2):
     s = '*' * i
     print(f'{s:^{num}}')
-for i in range(num,0,-2):
+for i in range(num,-1,-2):
     s = '*' * i
     print(f'{s:^{num}}')
     
+print()
